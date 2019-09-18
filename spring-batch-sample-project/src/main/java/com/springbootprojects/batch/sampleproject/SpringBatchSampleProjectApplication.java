94b760e2a1e50a5d162940c82bbb16371f9ddf2d
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-09-18 13:05:28 +0530

    Spring batch sample project

---
+++ spring-batch-sample-project/src/main/java/com/springbootprojects/batch/sampleproject/SpringBatchSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springbootprojects.batch.sampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchSampleProjectApplication.class, args);
+	}
+
+}
---
+++ spring-batch-sample-project/src/test/java/com/springbootprojects/batch/sampleproject/SpringBatchSampleProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springbootprojects.batch.sampleproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBatchSampleProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
