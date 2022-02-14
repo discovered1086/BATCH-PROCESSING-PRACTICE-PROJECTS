bd293538de97e9cfc299e3004e67908758a61aab
Kingshuk Mukherjee <sly.mania42@gmail.com>
2022-02-13 19:44:44 -0600

    Second project created for Kevin's course

---
+++ kevin-spring-batch-second/src/main/java/com/kingshuk/batchprocessing/kevinspringbatchsecond/KevinSpringBatchSecondApplication.java
@@ -0,0 +1,13 @@
+package com.kingshuk.batchprocessing.kevinspringbatchsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinSpringBatchSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KevinSpringBatchSecondApplication.class, args);
+    }
+
+}
---
+++ kevin-spring-batch-second/src/test/java/com/kingshuk/batchprocessing/kevinspringbatchsecond/KevinSpringBatchSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingshuk.batchprocessing.kevinspringbatchsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KevinSpringBatchSecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
